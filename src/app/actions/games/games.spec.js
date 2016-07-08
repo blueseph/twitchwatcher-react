b9c59bf70e39d7d6ed86f2e2f36697ba2ad7fc41
Daniel <dablanco@ea.com>
2016-07-08 13:00:25 -0400

    Removed copy/pasted tests. will fix later

--- src/app/actions/games/games.spec.js
+++ src/app/actions/games/games.spec.js
@@ -1,24 +1,24 @@
-import * as utils from '../../utils/TwitchUtils';
-import * as types from '../../constants/RequestTypes';
-
-describe('games actions', () => {
-  it('fetch should create FILTER_BY_NAME action', () => {
-    expect(actions.filterName('dota')).toEqual({
-      type: types.FILTER_BY_NAME,
-      term: 'dota',
-    });
-  });
-
-  it('filterGame should create FILTER_BY_GAME action', () => {
-    expect(actions.filterGame('League of Legends')).toEqual({
-      type: types.FILTER_BY_GAME,
-      term: 'League of Legends',
-    });
-  });
-
-  it('removeFilter should create FILTER_BY_GAME action', () => {
-    expect(actions.removeFilter()).toEqual({
-      type: types.REMOVE_FILTER,
-    });
-  });
-});
+// import * as utils from '../../utils/TwitchUtils';
+// import * as types from '../../constants/RequestTypes';
+//
+// describe('games actions', () => {
+//   it('fetch should create FILTER_BY_NAME action', () => {
+//     expect(actions.filterName('dota')).toEqual({
+//       type: types.FILTER_BY_NAME,
+//       term: 'dota',
+//     });
+//   });
+//
+//   it('filterGame should create FILTER_BY_GAME action', () => {
+//     expect(actions.filterGame('League of Legends')).toEqual({
+//       type: types.FILTER_BY_GAME,
+//       term: 'League of Legends',
+//     });
+//   });
+//
+//   it('removeFilter should create FILTER_BY_GAME action', () => {
+//     expect(actions.removeFilter()).toEqual({
+//       type: types.REMOVE_FILTER,
+//     });
+//   });
+// });
