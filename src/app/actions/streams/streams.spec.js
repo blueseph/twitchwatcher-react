fafb218f49ebaadc5f1a4c5a851b1e2c7c8d46b9
Daniel <dablanco@ea.com>
2016-07-11 14:49:17 -0400

    Added streams async action tests

--- src/app/actions/streams/streams.spec.js
+++ src/app/actions/streams/streams.spec.js
@@ -1,6 +1,9 @@
 import * as types from '../../constants/RequestTypes';
 import * as aTypes from '../../constants/ActionTypes';
 import * as actions from './streams';
+import * as utils from '../../utils/TwitchUtils';
+import { mockStore } from '../../utils/TestUtils';
+
 
 describe('filter actions', () => {
   describe('hideStreams', () => {
@@ -22,6 +25,34 @@ describe('filter actions', () => {
       const stream = { name: 'WinterSC' };
 
       expect(actions.selectStream(stream)).toEqual({ type: aTypes.SELECT_STREAM, stream });
+    });
+  });
+
+  describe('fetch', () => {
+    it('should create a FETCH_GAMES_SUCCESS when fetching games is successful', done => {
+      const streams = { streams: [{ channel: { display_name: 'Sodapoppin' } }] };
+      spyOn(utils, 'twitchEndpointFinder').and.returnValue(Promise.resolve('doesnt matter'));
+      spyOn(utils, 'getTwitchData').and.returnValue(Promise.resolve(streams));
+
+      const expectedActions = [
+        { type: types.FETCH_STREAMS_REQUEST },
+        { type: types.FETCH_STREAMS_SUCCESS, data: streams.streams },
+      ];
+
+      const store = mockStore({}, expectedActions, done, expect);
+      store.dispatch(actions.fetch());
+    });
+
+    it('should create a FETCH_GAMES_FAILURE when fetching games fails', done => {
+      spyOn(utils, 'twitchEndpointFinder').and.returnValue(Promise.reject());
+
+      const expectedActions = [
+        { type: types.FETCH_STREAMS_REQUEST },
+        { type: types.FETCH_STREAMS_FAILURE },
+      ];
+
+      const store = mockStore({}, expectedActions, done, expect);
+      store.dispatch(actions.fetch());
     });
   });
 });
