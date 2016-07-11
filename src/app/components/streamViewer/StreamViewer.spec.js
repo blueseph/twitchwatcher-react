5419b93fc335dff7ff12cbb0cf80c45ee7e8b099
Daniel <dablanco@ea.com>
2016-07-11 17:43:57 -0400

    Added tests for stream viewer

---
+++ src/app/components/streamViewer/StreamViewer.spec.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { StreamViewer } from './StreamViewer';
+import { shallow } from 'enzyme';
+
+function setup(override) {
+  const props = Object.assign({}, {
+    dispatch: jasmine.createSpy(),
+    selected: {
+      stream: {
+        _id: 123,
+        channel: {
+          name: 'Reckful',
+        },
+      },
+    },
+    actions: {
+      filterName: jasmine.createSpy(),
+    },
+  }, override);
+
+  const wrapper = shallow(<StreamViewer {...props} />);
+
+  return {
+    props,
+    wrapper,
+  };
+}
+
+describe('streamviewer component', () => {
+  it('should render itself', () => {
+    const { wrapper } = setup();
+
+    expect(wrapper.find('iframe').length).toBe(1);
+  });
+
+  it('should render nothing if there is no selected stream', () => {
+    const { wrapper } = setup({ selected: { stream: 0 } });
+
+    expect(wrapper.find('iframe').length).toBe(0);
+  });
+});
