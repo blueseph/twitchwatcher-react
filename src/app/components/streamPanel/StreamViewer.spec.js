d8b07399282c0e067c54b1c8010f4a6746ed078e
Daniel <dablanco@ea.com>
2016-07-12 12:03:17 -0400

    hilariously i rewrote one of the test files with stream chat. reverting

--- src/app/components/streamPanel/StreamViewer.spec.js
+++ src/app/components/streamPanel/StreamViewer.spec.js
@@ -1,25 +1,38 @@
-/* eslint no-underscore-dangle:0 */
-/* twitch natively has underscores as ids. nothing we can do */
-
-import React, { PropTypes } from 'react';
-
+import React from 'react';
 import { StreamViewer } from './StreamViewer';
-import { StreamChat } from './StreamChat';
+import { shallow } from 'enzyme';
 
-const StreamPanel = ({
-  selected,
-}) =>
-  <div className="stream-panel">
-    <StreamViewer
-      selected={selected}
-    />
-    <StreamChat
-      selected={selected}
-    />
-  </div>;
+function setup(override) {
+  const props = Object.assign({}, {
+    dispatch: jasmine.createSpy(),
+    selected: {
+      stream: {
+        _id: 123,
+        channel: {
+          name: 'Reckful',
+        },
+      },
+    },
+  }, override);
 
-StreamPanel.propTypes = {
-  selected: PropTypes.object.isRequired,
-};
+  const wrapper = shallow(<StreamViewer {...props} />);
 
-export { StreamPanel };
+  return {
+    props,
+    wrapper,
+  };
+}
+
+describe('streamviewer component', () => {
+  it('should render itself', () => {
+    const { wrapper } = setup();
+
+    expect(wrapper.find('iframe').length).toBe(1);
+  });
+
+  it('should render nothing if there is no selected stream', () => {
+    const { wrapper } = setup({ selected: { stream: 0 } });
+
+    expect(wrapper.find('iframe').length).toBe(0);
+  });
+});
