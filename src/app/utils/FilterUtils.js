6343eafdc77c4968d0a407733c4e46a49acb4a13
Daniel <dablanco@ea.com>
2016-07-18 14:03:34 -0400

    fixed an issue where the gop convention literally broke my player by not having a game associated with it

--- src/app/utils/FilterUtils.js
+++ src/app/utils/FilterUtils.js
@@ -1,6 +1,7 @@
 export const filterStreams = (streams, { term }) =>
   streams.filter(stream =>
-    (stream.channel.display_name.toLowerCase().includes(term.toLowerCase()) ||
-    stream.channel.status.toLowerCase().includes(term.toLowerCase()) ||
-    stream.game.toLowerCase().includes(term.toLowerCase())
-  ));
+    (stream.channel.display_name && stream.channel.display_name.toLowerCase()
+                                      .includes(term.toLowerCase())) ||
+    (stream.channel.status && stream.channel.status.toLowerCase().includes(term.toLowerCase())) ||
+    (stream.game && stream.game.toLowerCase().includes(term.toLowerCase()))
+);
